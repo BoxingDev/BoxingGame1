774b4f9db1d8b251543a32c5a4c7a699e27defb7
shamedark <flygaming1881@gmail.com>
2022-06-24 12:22:24 +0300

    Initial commit

---
+++ Source/BoxingGame/BoxingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BoxingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOXINGGAME_API ABoxingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
